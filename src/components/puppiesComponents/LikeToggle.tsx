9d1054c1f9c7a86abef8d58fd200a7fc1037940a
griminir <viktor.elliot.king@gmail.com>
2025-04-10 12:31:22 +0200

    added fake delay and a pending state to simulate api call

--- src/components/puppiesComponents/LikeToggle.tsx
+++ src/components/puppiesComponents/LikeToggle.tsx
@@ -1,6 +1,7 @@
-import { Heart } from 'lucide-react';
+import { Heart, LoaderCircle } from 'lucide-react';
 import type { Puppy } from '@/types';
 import { useLiked } from '@/context/Liked-Context.ts';
+import { useState } from 'react';
 
 type LikeToggleProps = {
   puppyId: Puppy['id'];
@@ -8,25 +9,34 @@ type LikeToggleProps = {
 
 export default function LikeToggle({ puppyId }: LikeToggleProps) {
   const { liked, setLiked } = useLiked();
+  const [pending, setPending] = useState(false);
   const handleLike = () => {
-    if (liked.includes(puppyId)) {
-      setLiked((prev: Puppy['id'][]) =>
-        prev.filter((id: Puppy['id']) => id !== puppyId),
-      );
-    } else {
-      setLiked((prev: Puppy['id'][]) => [...prev, puppyId]);
-    }
+    setPending(true);
+    setTimeout(() => {
+      if (liked.includes(puppyId)) {
+        setLiked((prev: Puppy['id'][]) =>
+          prev.filter((id: Puppy['id']) => id !== puppyId),
+        );
+      } else {
+        setLiked((prev: Puppy['id'][]) => [...prev, puppyId]);
+      }
+      setPending(false);
+    }, 1500);
   };
 
   return (
     <button className='group' onClick={handleLike}>
-      <Heart
-        className={
-          liked.includes(puppyId)
-            ? 'fill-pink-500 stroke-none'
-            : 'stroke-slate-200 group-hover:stroke-slate-300'
-        }
-      />
+      {pending ? (
+        <LoaderCircle className='animate-spin stroke-slate-300' />
+      ) : (
+        <Heart
+          className={
+            liked.includes(puppyId)
+              ? 'fill-pink-500 stroke-none'
+              : 'stroke-slate-200 group-hover:stroke-slate-300'
+          }
+        />
+      )}
     </button>
   );
 }
