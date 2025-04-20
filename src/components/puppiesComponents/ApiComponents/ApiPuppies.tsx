7475f7fe857f5c40c1462125f0599456086f9c58
griminir <viktor.elliot.king@gmail.com>
2025-04-20 19:56:00 +0200

    just some quick error handling client side, but I need to rework some of the code to make it show up after it refeshes the data

--- src/components/puppiesComponents/ApiComponents/ApiPuppies.tsx
+++ src/components/puppiesComponents/ApiComponents/ApiPuppies.tsx
@@ -1,10 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { LoaderCircle } from 'lucide-react';
 
 export default function ApiPuppies() {
-  useEffect(() => {}, []);
+  const [apiPuppies, setApiPuppies] = useState<[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>('');
+  useEffect(() => {
+    async function getPuppies() {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8000/api/puppies');
+        if (!response.ok) {
+          const errorData = await response.json();
+          setError(`${errorData.message}: ${errorData.details}`);
+          throw errorData;
+        }
+        const data = await response.json();
+        setApiPuppies(data);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+
+    getPuppies();
+  }, []);
+
   return (
     <div className='mt-12 bg-white p-6 shadow ring ring-black/5'>
-      <p>Api puppies</p>
+      {loading && <LoaderCircle className='animate-spin stroke-slate-300' />}
+      {apiPuppies.length > 0 && (
+        <pre>{JSON.stringify(apiPuppies, null, 2)}</pre>
+      )}
+      {error && <p className='text-red-500'>{error}</p>}
     </div>
   );
 }
